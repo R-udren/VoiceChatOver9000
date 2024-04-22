f2cc472b89094da4a138cd3d033745fc0364908b
rovert <nothing@not.not>
2024-04-22 22:04:03 +0300

    Created main func

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
-
 from rich.console import Console
 from openai import OpenAI
 
 from ai_assistant import AIAssistant
 from config import OPENAI_API_KEY
 
-
 console = Console(style="bold cyan")
 openai_client = OpenAI(api_key=OPENAI_API_KEY)
 
-if __name__ == "__main__":
+
+def main():
     assistant = AIAssistant(console, openai_client)
 
     console.clear()
     console.print("[blue]:wave: Welcome to the rovert's AI Assistant chat!\n"
-                  "[yellow]:information: Empty input will trigger the microphone.\n"
+                  "[yellow]:information:  Empty input will trigger the microphone.\n"
                   "[red]:exclamation: Press CTRL+C to exit.")
 
     assistant.main()
+
+
+if __name__ == "__main__":
+    main()
